df2371beb4b6ac6d702f57479fcb2bf87fab9469
franklin <83294356+frankcodedKing@users.noreply.github.com>
2023-02-14 19:30:09 +0100

    First commit for Mintable

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,27 @@
 require('@nomiclabs/hardhat-waffle')
 require('dotenv').config()
 
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "dnH-fo09NOGJYIz8HS30srFi5UuJ79VA";
+
+const GOERLI_PRIVATE_KEY = "7679812482cf4665db27548631bb7ed541faeb1970421b4d383a9ecd36e82daf";
+
+
+
 module.exports = {
   defaultNetwork: 'localhost',
   networks: {
     localhost: {
       url: 'http://127.0.0.1:8545',
     },
+    goerli: {
+      url: process.env.ALCHEMY_GOERLI_URL,
+      // ℹ️ Added our new account to the Rinkeby settings
+      accounts: [process.env.GOERLI_WALLET_PRIVATE_KEY],
+    }
+  
   },
   solidity: {
     version: '0.8.11',
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,14 +2,14 @@ const { ethers } = require('hardhat')
 const fs = require('fs')
 
 async function main() {
-  const contract_name = ''
-  const Contract = await ethers.getContractFactory(contract_name)
-  const contract = await Contract.deploy()
+  const base_uri = 'https://ipfs.io/ipfs/QmY86YA9G13BiJcmY6cLCakkUBTge8yCCzmVbLhGg2vj4e/'
+  const Contract = await ethers.getContractFactory('Mintable')
+  const contract = await Contract.deploy('Mintable NFT', 'MTBL', base_uri)
 
   await contract.deployed()
 
   const address = JSON.stringify({ address: contract.address }, null, 4)
-  fs.writeFile('./src/abis/contractAddress.json', address, 'utf8', (err) => {
+  fs.writeFile('./src/abis/contractAddress.json', address, 'utf8', (err) => { 
     if (err) {
       console.error(err)
       return
